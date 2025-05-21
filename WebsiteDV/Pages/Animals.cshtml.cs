788d53a03c91ef6bca57208d52d858ec3f83815b
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-05-21 11:40:06 +0200

    Pages added

--- WebsiteDV/Pages/Animals.cshtml.cs
+++ WebsiteDV/Pages/Animals.cshtml.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LibraryDV.Services;
+using LibraryDV.Models;
+using LibraryDV.Repos;
 
 namespace WebsiteDV.Pages
 {
     public class AnimalsModel : PageModel
     {
+        public List<Animal> Animals { get; set; }
+
         public void OnGet()
         {
         }
