7e21dfd7d588674a3362c05d2d182eb2fcedb1c3
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-05-23 11:19:37 +0200

    IndexSetup groundworks

--- WebsiteDV/Pages/Index.cshtml.cs
+++ WebsiteDV/Pages/Index.cshtml.cs
@@ -1,3 +1,6 @@
+using LibraryDV.Repos;
+using LibraryDV.Services;
+using LibraryDV.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,14 +9,21 @@ namespace WebsiteDV.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private static IBlogPostRepo _blogPostRepo;
+    private BlogPostService _blogPostService;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    public List<BlogPost> BlogPosts { get; private set; }
+
+    public IndexModel(ILogger<IndexModel> logger, IBlogPostRepo bPRepo, BlogPostService bPServ)
     {
         _logger = logger;
+        _blogPostRepo = bPRepo;
+        _blogPostService = bPServ;
     }
 
     public void OnGet()
     {
-
+        BlogPosts = _blogPostService.GetAllBlogPosts();
     }
 }
