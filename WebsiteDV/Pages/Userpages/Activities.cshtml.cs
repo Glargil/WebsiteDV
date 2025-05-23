905f02fad5447885e702c37d8903ca10bc6bd51e
mak025 <mak025@edu.zealand.dk>
2025-05-23 12:26:34 +0200

    Navbar Semi-Complete

--- WebsiteDV/Pages/Userpages/Activities.cshtml.cs
+++ WebsiteDV/Pages/Userpages/Activities.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebsiteDV.Pages
+namespace WebsiteDV.Pages.Userpages
 {
     public class ActivitiesModel : PageModel
     {
