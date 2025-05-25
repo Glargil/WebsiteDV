937dec046304bfd1e8448e9fa24c9605c854526b
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-05-25 12:34:53 +0200

    BlogPost Create and Delete functional

--- WebsiteDV/Pages/Administration/BlogPostAdministration.cshtml.cs
+++ WebsiteDV/Pages/Administration/BlogPostAdministration.cshtml.cs
@@ -1,3 +1,6 @@
+using LibraryDV.Repos;
+using LibraryDV.Services;
+using LibraryDV.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,40 @@ namespace WebsiteDV.Pages.Administration
 {
     public class BlogPostAdministrationModel : PageModel
     {
+        IBlogPostRepo _blogPostRepo = new BlogPostRepo();
+        BlogPostService _blogPostService;
+
+        [BindProperty]
+        public List<BlogPost> BlogPosts { get; set; }
+        [BindProperty]
+        public string Title { get; set; }
+        [BindProperty]
+        public string ImgPath { get; set; } = string.Empty;
+        [BindProperty]
+        public string Text { get; set; }
+        [BindProperty]
+        public int ToDelete { get; set; }
+
+        public BlogPostAdministrationModel(IBlogPostRepo blogPostRepo)
+        {
+            _blogPostRepo = blogPostRepo;
+            _blogPostService = new BlogPostService(_blogPostRepo);
+        }
         public void OnGet()
         {
+            BlogPosts = _blogPostService.GetAllBlogPosts();
+        }
+
+        public IActionResult OnPostCreate()
+        {
+            _blogPostService.CreateBlogPost(Title, ImgPath, Text);
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostDelete()
+        {
+            _blogPostService.DeleteBlogPost(ToDelete);
+            return RedirectToPage();
         }
     }
 }
