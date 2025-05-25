fbb8b0f2f433ecea959dea889b95a2e1f3b862cc
mak025 <mak025@edu.zealand.dk>
2025-05-25 13:19:32 +0200

    EditCustomer Functionality

--- WebsiteDV/Pages/Administration/EditCustomer.cshtml.cs
+++ WebsiteDV/Pages/Administration/EditCustomer.cshtml.cs
@@ -1,12 +1,63 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LibraryDV.Services;
+using LibraryDV.Models;
+using System.Linq;
 
 namespace WebsiteDV.Pages.Administration
 {
     public class EditCustomerModel : PageModel
     {
-        public void OnGet()
+        private readonly UserServices _userServices;
+
+        public EditCustomerModel(UserServices userServices)
         {
+            _userServices = userServices;
+        }
+
+        [BindProperty]
+        public int UserID { get; set; }
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public string PhoneNumber { get; set; }
+        [BindProperty]
+        public string Email { get; set; }
+
+        public string ErrorMessage { get; set; }
+        public string SuccessMessage { get; set; }
+
+        public IActionResult OnGet(int userId)
+        {
+            var user = _userServices.GetUserID(userId);
+            if (user == null || user.Type.ToString() != "Customer")
+            {
+                ErrorMessage = "Kunden blev ikke fundet.";
+                return Page();
+            }
+
+            UserID = user.UserID;
+            Name = user.Name;
+            PhoneNumber = user.PhoneNumber;
+            Email = user.Email;
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (string.IsNullOrWhiteSpace(Name) ||
+                string.IsNullOrWhiteSpace(PhoneNumber) ||
+                string.IsNullOrWhiteSpace(Email))
+            {
+                ErrorMessage = "Alle felter skal udfyldes.";
+                return Page();
+            }
+
+            _userServices.EditUser(UserID, Name, PhoneNumber, Email);
+
+            SuccessMessage = "Kundeoplysninger er opdateret!";
+            return RedirectToPage("/Administration/AllCustomers"); // Or wherever you want to redirect
         }
     }
 }
